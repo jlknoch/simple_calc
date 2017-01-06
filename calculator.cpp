d9369b1df956bcde03ffadbed17fd44f966fb5cc
Jan-Lukas Knoch <jl_knoch@protonmail.com>
2017-01-06 03:41:46 +0100

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,41 @@
-#include <iostream>
+#include <iostream> 
 
-using namespace std;
+using namespace std; 
 
-int main(){
+int main()
+{
 
-double input1, input2, output;
+double input1, input2, output;        
 char inputchar;
-cout << "Enter calculation: ";
-cin >> input1 >> inputchar >> input2;
+cout << "Enter calculation: ";        
+cin >> input1 >> inputchar >> input2; // GET CALCULATION
 
-switch(inputchar){
-case '+': output = input1 + input2; break;
-case '-': output = input1 - input2; break;
-case '*': output = input1 * input2; break;
-case '/': output = input1 / input2; break;
-default: cout << "Cyka Blyat... learn writing\n"; return 1;
+  
+switch(inputchar)   //RECOGNIZE THE CALCULATION CHAR (+,-,*,/)
+{
+  case '+': output = input1 + input2; 
+    break;
+  case '-': output = input1 - input2; 
+    break;
+  case '*': output = input1 * input2; 
+    break;
+  case '/': output = input1 / input2; 
+    break; 
+  default: cout << "Calculation not valid.\n"; 
+    return 1; // TELLS THE SYSTEM THAT THERE WAS AN ERROR IN THE CODE
 }
-cout << "Ergebnis: " << output << endl;
-return 0;
+  
+  
+cout << "Ergebnis: " << output << endl; // OUTPUTS RESULT
+return 0; // TELLS THE SYSTEM THAT EVERYTHING WORKED SUCCESSFUL -> PROGRAM FINISHED
+  
 }
+
+/*  COPYLEFT
+  
+    AUTHOR: 
+    Jan-Lukas Knoch
+    MAIL: jl_knoch@protonmail.com
+    GITHUB: https://github.com/jlknoch/
+  
+*/
